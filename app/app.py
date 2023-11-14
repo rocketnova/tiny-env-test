6492c104ac5672974c0628761a6f10fa42ed25e3
Rocket ! <rocketnova@users.noreply.github.com>
2023-11-13 16:08:38 -0800

    Create tiny test app to verify env vars and secrets are correctly hooked up.

---
+++ app/app.py
@@ -0,0 +1,34 @@
+import logging
+import os
+
+from flask import Flask
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+app = Flask(__name__)
+
+def main():
+    host = os.environ.get("HOST")
+    port = os.environ.get("PORT")
+    logger.info("Running Flask app on host %s and port %s", host, port)
+    app.run(host=host, port=port)
+
+
+@app.route("/")
+def hello_world():
+    custom_env_var = os.environ.get("CUSTOM_ENV_VAR")
+    custom_secret = os.environ.get("CUSTOM_SECRET")
+    return f"<p>Hello, World!</p><p>Custom env var: {custom_env_var}</p><p>Custom secret: {custom_secret}"
+
+
+@app.route("/customhealth")
+def health():
+    conn = get_db_connection()
+    conn.execute("SELECT 1")
+    return "OK"
+
+
+if __name__ == "__main__":
+    main()
